df09629679679405086dfcb97da2dda689e1b9fb
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-02-22 16:47:38 -0800

    compiling to test on github as preview seems busted

---
+++ script.js
@@ -0,0 +1,6 @@
+let zombie = prompt("Are you a zombie that eats brains?")
+    if(zombie == yes)
+        {document.write("We have alerted the authorities. (Just Kidding! Or am I?)")}
+    else if(zombie == no)
+        {document.write("Whew, thats excellent news!")}
+    else{document.write("That was a yes or no question, silly goose.")}
