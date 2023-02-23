3cabe1d0715e3dc1f53fbb7e5bf4e87bc97d8be7
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-02-22 17:11:14 -0800

    got the script to work! thanks Kass

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let zombie = prompt("Are you a zombie that eats brains?")
-    if(zombie == yes)
-        {document.write("We have alerted the authorities. (Just Kidding! Or am I?)")}
-    else if(zombie == no)
-        {document.write("Whew, thats excellent news!")}
-    else{document.write("That was a yes or no question, silly goose.")}
+alert("The year is 1894...the zombie outbreak has arrived!")
+
+
+let zombie = prompt("Are you a zombie that eats brains? [Lowercase please!]")
+    if(zombie == "yes")
+        {document.write("We have alerted the authorities. [Just Kidding! Or am I?]")}
+    else if(zombie == "no")
+        {document.write("Whew, thats excellent news!")} 
+    else{
+        document.write("That was a yes or no question, silly goose.")}
+
+console.log("Are they a zombie? " + zombie)
+
+confirm("Are you 100% sure about that?")
