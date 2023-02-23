e90a31ba93449ee77e9c4e660a5b4831f279e963
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-02-23 14:36:27 -0800

    cant remember but it seemed like i should commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function greeting(){
+function greeting() {
     alert("The year is 1894...the zombie outbreak has arrived!")
 }
 
-function infection(){
+function infection() {
     let zombie = prompt("Are you a zombie that eats brains?")
     console.log(zombie);
-    if(zombie == null){
-        infection()}
-    else if(zombie.toLowerCase() == "yes")
-        {document.write("We have alerted the authorities. Don't read this website, zombie! [Just Kidding! Or am I?]")}
-    else if(zombie.toLowerCase() == "no")
-        {document.write("Whew, thats excellent news! Study this website carefully, fellow human.")} 
-    else{
+    if (zombie == null) {
+        infection()
+    }
+    else if (zombie.toLowerCase() == "yes") { document.write("We have alerted the authorities. Don't read this website, zombie! [Just Kidding! Or am I?]") }
+    else if (zombie.toLowerCase() == "no") { document.write("Whew, thats excellent news! Study this website carefully, fellow human.") }
+    else {
         alert("That was a yes or no question, silly goose. Try again.")
-        infection()}
+        infection()
+    }
 }
 
-function doublecheck(){
-    if(confirm("Are you 100% sure about that?") == true){}
+function doublecheck() {
+    if (confirm("Are you 100% sure about that?") == true) { }
     else {
         alert("Are you not sure of yourself? Try again.")
         infection()
@@ -25,4 +25,3 @@ function doublecheck(){
     }
 
 }
-
