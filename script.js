adeadc9a9fe373d1cb24b764b282273ef0f15038
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-02-23 12:39:11 -0800

    reformatted all JS into functions

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-alert("The year is 1894...the zombie outbreak has arrived!")
+function greeting(){
+    alert("The year is 1894...the zombie outbreak has arrived!")
+}
 
-
-let zombie = prompt("Are you a zombie that eats brains? [Lowercase please!]")
-    if(zombie == "yes")
+function infection(){
+    let zombie = prompt("Are you a zombie that eats brains?")
+    if(zombie == null){
+        infection()}
+    else if(zombie.toLowerCase() == "yes")
         {document.write("We have alerted the authorities. Don't read this website, zombie! [Just Kidding! Or am I?]")}
-    else if(zombie == "no")
+    else if(zombie.toLowerCase() == "no")
         {document.write("Whew, thats excellent news! Study this website carefully, fellow human.")} 
     else{
-        document.write("That was a yes or no question, silly goose.")}
+        alert("That was a yes or no question, silly goose. Try again.")
+        infection()}
+}
 
-console.log("Are they a zombie? " + zombie)
+function doublecheck(){
+    if(confirm("Are you 100% sure about that?") == true){}
+    else {
+        alert("Are you not sure of yourself? Try again.")
+        infection()
+        doublecheck()
+    }
 
-confirm("Are you 100% sure about that?")
+}
+
